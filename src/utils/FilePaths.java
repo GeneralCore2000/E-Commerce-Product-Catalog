c9d9ab17f59165e7f4087c24a9a8a74bd976b05b
J.L. Vargas <78johnlloyd@gmail.com>
2025-11-08 12:39:24 +0800

    Add pending orders file

--- src/utils/FilePaths.java
+++ src/utils/FilePaths.java
@@ -3,5 +3,6 @@ package utils;
 public class FilePaths {
     public static final String USER_ACCOUNTS = "src\\datarepositories\\user_accounts.txt";
     public static final String PRODUCTS = "src\\datarepositories\\products.txt";
-    public static final String[] ALL_FILES = {USER_ACCOUNTS, PRODUCTS};
+    public static final String PENDING_ORDERS = "src\\datarepositories\\pending_orders.txt";
+    public static final String[] ALL_FILES = {USER_ACCOUNTS, PRODUCTS, PENDING_ORDERS};
 }
